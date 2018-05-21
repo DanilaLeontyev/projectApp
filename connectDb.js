dc3ebeb52f47b9f868ccd5eb7cdd50d7b4212509
Danila Leontyev <danila.leontyev.web@gmail.com>
2018-05-21 20:02:23 +0500

    first commit

---
+++ connectDb.js
@@ -0,0 +1,25 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('./db/test.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('connect to db, success');
+});
+
+db.serialize(() => {
+    db.each(`SELECT userId as id, Name as name from users`, (err, row) => {
+        if (err) {
+            return console.error(err.message);
+        }
+
+        console.log(row.id + '\t' + row.name);
+    });
+});
+
+db.close(err => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('close the database connection');
+});
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json();
+});
+app.listen(3000, () => {
+    console.log('Server is listenint on port 3000');
+});
